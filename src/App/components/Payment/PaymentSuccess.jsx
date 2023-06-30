32d2b182d95bc2c3fdd6d5ff09f2f1a3697fe227
RameshAnnabhimoju <ramesh.annabhimoju@gmail.com>
2023-06-30 12:41:05 +0530

    removed cors temporarily

--- src/App/components/Payment/PaymentSuccess.jsx
+++ src/App/components/Payment/PaymentSuccess.jsx
@@ -1,17 +1,26 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 function PaymentSuccess() {
+  const navigate = useNavigate();
   const location = useLocation();
   return (
     <div className="paymentSucess-container">
       <div className="paymentSuccess-card">
         The transaction ID for your payment of <b>₹ {location.state.amount}</b>{" "}
         was successfully saved.
-        <br /> Please save the following reference number:
+        {/* <br /> Please save the following reference number:
         <br />
         <b> {location.state.referenceId} </b>
-        <br />
+        <br /> */}
+        <button
+          className="btn btn-primary"
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          Go to Home
+        </button>
       </div>
     </div>
   );
